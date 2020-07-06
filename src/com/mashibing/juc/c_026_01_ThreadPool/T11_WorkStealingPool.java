a0dad31a6c57bd5e5fd4c2f9a1d26cb0646d147e
liuyang <liuyang@coohua.com>
2020-07-06 20:31:24 +0800

    workStealingPool

---
+++ src/com/mashibing/juc/c_026_01_ThreadPool/T11_WorkStealingPool.java
@@ -0,0 +1,43 @@
+package com.mashibing.juc.c_026_01_ThreadPool;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class T11_WorkStealingPool {
+    public static void main(String[] args) throws IOException {
+        ExecutorService service = Executors.newWorkStealingPool();
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+        service.execute(new R(1000));
+        service.execute(new R(2000));
+        service.execute(new R(2000));
+        service.execute(new R(2000));
+        service.execute(new R(2000));
+
+        System.in.read();
+    }
+
+    static class R implements Runnable {
+
+        int time;
+
+        R(int t) {
+            this.time = t;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                TimeUnit.MILLISECONDS.sleep(time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(time + " " + Thread.currentThread().getName());
+        }
+    }
+
+}
